bf1d10dd3f83a6447dcf8fdef685a99d24979c01
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-11-27 11:15:27 +0100

    https://next-auth.js.org/getting-started/example

--- pimpuszek/README.md
+++ pimpuszek/README.md
@@ -1,3 +1,10 @@
 # https://www.youtube.com/watch?v=xXwxEudjiAY&list=PLxCkFZQohykk8ejbV94XbigHSONNq4m3C
 
 - npm run dev
+
+# https://www.youtube.com/watch?v=DfIDr2jCkHo&list=PLxCkFZQohyklNCNiqx-mJ5IiKeRgHaGvn
+
+## 1. https://youtu.be/DfIDr2jCkHo
+- https://www.npmjs.com/package/next-auth
+- npm i next-auth
+- https://next-auth.js.org/getting-started/example
---
+++ pimpuszek/pages/api/auth/[...nextauth].js
@@ -0,0 +1,15 @@
+// https://next-auth.js.org/getting-started/example#add-api-route
+
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // ...add more providers here
+  ],
+};
+export default NextAuth(authOptions);
