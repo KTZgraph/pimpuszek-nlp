cd0fc24007998d0252c9aaebaf96b5e197ef7314
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-11-27 11:11:38 +0100

    koniec https://youtu.be/xXwxEudjiAY

--- pimpuszek/app/posts/[id]/page.js
+++ pimpuszek/app/posts/[id]/page.js
@@ -1,10 +1,24 @@
+// do wywoływania funckji, któa pobiera dane
+import { use } from "react";
+
 // WARNING - nazwa folder [id] bo to dynamiczne, ale plik page.js
 // wyświetlanie posta po jego id - id  jest z urla
 
+async function getPost(id) {
+  let post = await fetch(`https://dummyjson.com/posts/${id}`);
+  return post.json();
+}
+
 const page = ({ params }) => {
   let id = params.id;
+  let post = use(getPost(id));
 
-  return <div>post id klinketo: {id}</div>;
+  return (
+    <div>
+      <p>{post.title}</p>
+      <p>{post.body}</p>
+    </div>
+  );
 };
 
 export default page;
