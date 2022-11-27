8edd0b42529f2ead2906ff9ca7cd970c318e3138
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-11-27 10:30:42 +0100

    https://youtu.be/xXwxEudjiAY?t=249

--- pimpuszek/app/layout.tsx
+++ pimpuszek/app/layout.tsx
@@ -1,12 +1,24 @@
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body>
+        {/* dodanie nawigacji do layoutu */}
+        <nav style={{ padding: "10px 0 10px 0" }}>
+          <a href="/" style={{ padding: "0 5px 0 0" }}>
+            Home
+          </a>
+          <a href="/posts" style={{ padding: "0 5px 0 0" }}>
+            Posts
+          </a>
+        </nav>
+
+        {children}
+      </body>
     </html>
-  )
+  );
 }
