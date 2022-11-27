677f9437df2d51c18581ba8b69e42d46d3da34b6
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-11-27 11:20:31 +0100

    https://next-auth.js.org/getting-started/example

---
+++ pimpuszek/components/login-btn.jsx
@@ -0,0 +1,20 @@
+// https://next-auth.js.org/getting-started/example#frontend---add-react-hook
+
+import { useSession, signIn, signOut } from "next-auth/react";
+export default function Component() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  );
+}
