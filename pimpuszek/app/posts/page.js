289d2225158fadd71c5f5425707da4377d8bd7f5
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-11-27 10:42:06 +0100

    9:30 działa jako serwer komponent

--- pimpuszek/app/posts/page.js
+++ pimpuszek/app/posts/page.js
@@ -1,8 +1,18 @@
 // teraz zamiast nazw plików mamy foldery - pl;ik page.js musi wystąpić
+// zxamiast propsów - to use z reacta do pobierania danych w kopomenecie
+import { use } from "react";
 
-import React from "react";
+async function getPosts() {
+  let posts = await fetch("https://dummyjson.com/posts?limit=3");
+
+  return posts.json();
+}
 
 const page = () => {
+  let posts = use(getPosts());
+  //   wyświetla sie w konsoli serwera - ten komponent działa jako server komponent
+  console.log(posts);
+
   return (
     <div>
       <h1>Post page</h1>
