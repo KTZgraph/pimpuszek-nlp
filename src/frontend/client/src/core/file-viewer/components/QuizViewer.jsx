8e224809c8c690577a9debe16eebe277c955d118
pawlaczyk <dominika.pawlaczyk9@gmail.com>
2022-12-04 03:44:18 +0100

    losowy quiz

--- src/frontend/client/src/core/file-viewer/components/QuizViewer.jsx
+++ src/frontend/client/src/core/file-viewer/components/QuizViewer.jsx
@@ -2,30 +2,35 @@ import { useState, useEffect } from "react";
 
 import Quizz from "./Quiz";
 
-const QuizViewer = ({ fileData, handleMark }) => {
-  const [rowId, setRowId] = useState(0);
+const QuizViewer = ({ fileData, handleMark, randomRowNumber }) => {
+  // const [rowId, setRowId] = useState(0);
+
+  const [rowId, setRowId] = useState(randomRowNumber);
   const [quizzesTotal, setQuizzesTotal] = useState(0);
   const [currentQuiz, setCurrentQuiz] = useState(null);
 
   useEffect(() => {
     setQuizzesTotal(fileData.data.length);
-    setCurrentQuiz(fileData.data[0]);
+    setCurrentQuiz(fileData.data[randomRowNumber]);
   }, [fileData]);
 
   const handleNext = (mark) => {
     console.log("Odpowiedź mark: ", mark);
 
-    rowId < fileData.data.length - 1
-      ? setRowId(rowId + 1)
-      : setRowId(fileData.data.length - 1);
+    // rowId < fileData.data.length - 1
+    //   ? setRowId(rowId + 1)
+    //   : setRowId(fileData.data.length - 1);
 
     setCurrentQuiz(fileData.data[rowId]);
 
     // update API
     handleMark(rowId, mark);
+    setRowId(randomRowNumber);
   };
 
   if (!currentQuiz) return null;
+  console.log("rowId: ", rowId);
+  console.log("randomRowNumber: ", randomRowNumber);
 
   return (
     <div className="quiz-list">
